84c4658a110ed08c16057e282f91aa73082f69ec
Rakibul Hasan <xenax.rakibul@gmail.com>
2018-10-15 02:22:06 +0600

    Homepage

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Text, ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { createStackNavigator } from 'react-navigation';
-import { Card, Button, Icon } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
 
 import { getLaunches } from '../redux/launch';
+import FlightCard from './components/FlightCard';
 
 export class HomeScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -30,17 +31,7 @@ export class HomeScreen extends Component {
     return (
       <ScrollView>
         {!!this.props.launch.launches.length &&
-          this.props.launch.launches.map(item => (
-            <Card key={item.id} title={item.name} image={{ uri: item.rocket.imageURL }}>
-              <Text style={{ marginBottom: 10 }}>{item.missions[0].description}</Text>
-              <Button
-                icon={{ name: 'code' }}
-                backgroundColor="#03A9F4"
-                buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-                title="VIEW DETAILS"
-              />
-            </Card>
-          ))}
+          this.props.launch.launches.map(item => <FlightCard key={item.id} {...item} />)}
       </ScrollView>
     );
   }
---
+++ src/screens/components/FlightCard.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+import moment from 'moment';
+
+export default class FlightCard extends Component {
+  state = {
+    days: '',
+    hours: '',
+    minutes: '',
+    seconds: '',
+  };
+
+  componentDidMount() {
+    const eventTime = this.props.wsstamp; // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
+    const currentTime = moment().unix(); // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
+    const diffTime = eventTime - currentTime;
+    let duration = moment.duration(diffTime * 1000, 'milliseconds');
+    const interval = 1000;
+
+    setInterval(() => {
+      duration = moment.duration(duration - interval, 'milliseconds');
+      this.setState({
+        days: duration.days(),
+        hours: duration.hours(),
+        minutes: duration.minutes(),
+        seconds: duration.seconds(),
+      });
+    }, interval);
+  }
+
+  render() {
+    const { name, rocket, missions } = this.props;
+    const { days, hours, minutes, seconds } = this.state;
+    return (
+      <Card title={name} image={{ uri: rocket.imageURL }}>
+        <Text style={{ marginBottom: 10 }}>{missions[0].description}</Text>
+        <Text style={{ marginBottom: 10, textAlign: 'center' }}>Launching in...</Text>
+        <Text style={{ marginBottom: 10, fontSize: 24, textAlign: 'center' }}>
+          <Text>{!!days && `${days} days, `}</Text>
+          <Text>{`${hours} hours, `}</Text>
+          <Text>{`${minutes} minutes, `}</Text>
+          <Text>{`${seconds} seconds`}</Text>
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03A9F4"
+          buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+          title="VIEW DETAILS"
+        />
+      </Card>
+    );
+  }
+}
