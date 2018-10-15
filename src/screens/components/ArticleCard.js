e9077c337480228c080f101725bbd803e4c6988a
Rakibul Hasan <xenax.rakibul@gmail.com>
2018-10-15 18:19:56 +0600

    RocketCard & ArticleCard redisign

--- src/screens/components/ArticleCard.js
+++ src/screens/components/ArticleCard.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Card, Button } from 'react-native-elements';
+
+import { primaryColor } from '../../colors';
 
 export default class ArticleCard extends Component {
   render() {
     const { title, featured_image, news_site_long } = this.props; //eslint-disable-line
     return (
-      <Card title={title} image={{ uri: featured_image }}>
-        <Text style={{ marginBottom: 10 }}>{news_site_long}</Text>
-        <Button
-          icon={{ name: 'code' }}
-          backgroundColor="#03A9F4"
-          buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-          title="VIEW DETAILS"
-        />
+      <Card image={{ uri: featured_image }}>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ marginLeft: 10, flex: 1, flexWrap: 'wrap' }}>
+            <Text style={{ marginBottom: 10, fontSize: 20, flex: 1, flexWrap: 'wrap' }}>
+              From: {news_site_long}
+            </Text>
+            <Text style={{ marginBottom: 10, flex: 1, flexWrap: 'wrap' }}>{title}</Text>
+            <Button
+              icon={{ name: 'book', type: 'font-awesome5', color: primaryColor }}
+              title="READ MORE"
+              buttonStyle={{
+                backgroundColor: 'white',
+                borderColor: primaryColor,
+                borderWidth: 0,
+                borderRadius: 5,
+                padding: 5,
+              }}
+              containerViewStyle={{
+                marginLeft: 'auto',
+              }}
+              textStyle={{ color: primaryColor }}
+            />
+          </View>
+        </View>
       </Card>
     );
   }
--- src/screens/components/RocketCard.js
+++ src/screens/components/RocketCard.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import { Card, Button } from 'react-native-elements';
+import { Text, View } from 'react-native';
+import { Card, Button, Avatar } from 'react-native-elements';
+
+import { primaryColor } from '../../colors';
 
 export default class RocketCard extends Component {
   render() {
     const { name, configuration, imageURL, family } = this.props;
     return (
-      <Card title={name} image={{ uri: imageURL }}>
-        <Text style={{ marginBottom: 10 }}>{configuration}</Text>
-        <Text style={{ marginBottom: 10, textAlign: 'center' }}>{family && family.name}</Text>
+      <Card>
+        <View style={{ flexDirection: 'row' }}>
+          <Avatar large rounded source={{ uri: imageURL }} activeOpacity={0.7} />
+          <View style={{ marginLeft: 10 }}>
+            <Text style={{ marginBottom: 10, fontSize: 20 }}>{name}</Text>
+            <Text style={{ marginBottom: 10 }}>Configuration: {configuration}</Text>
+            <Text style={{ marginBottom: 10 }}>{family && `Family: ${family.name}`}</Text>
+          </View>
+        </View>
         <Button
-          icon={{ name: 'code' }}
-          backgroundColor="#03A9F4"
-          buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-          title="VIEW DETAILS"
+          icon={{ name: 'rocket', type: 'font-awesome', color: primaryColor }}
+          title="LEARN MORE"
+          buttonStyle={{
+            backgroundColor: 'white',
+            borderColor: primaryColor,
+            borderWidth: 0,
+            borderRadius: 5,
+            padding: 5,
+          }}
+          containerViewStyle={{
+            marginLeft: 'auto',
+          }}
+          textStyle={{ color: primaryColor }}
         />
       </Card>
     );
