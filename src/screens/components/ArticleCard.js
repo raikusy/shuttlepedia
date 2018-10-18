3c43e40788d86a0b9f093e283dcb6f314ba3fe9a
Rakibul Hasan <xenax.rakibul@gmail.com>
2018-10-18 22:38:57 +0600

    Add: Time & Link in Article

--- src/screens/components/ArticleCard.js
+++ src/screens/components/ArticleCard.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Linking } from 'react-native';
 import { Card, Button } from 'react-native-elements';
+import moment from 'moment';
 
 import { primaryColor } from '../../colors';
 
 export default class ArticleCard extends Component {
   render() {
-    const { title, featured_image, news_site_long } = this.props; //eslint-disable-line
+    const { title, featured_image, news_site_long, url, date_published } = this.props; //eslint-disable-line
     return (
       <Card image={{ uri: featured_image }}>
         <View style={{ flexDirection: 'row' }}>
           <View style={{ marginLeft: 10, flex: 1, flexWrap: 'wrap' }}>
             <Text style={{ marginBottom: 10, fontSize: 20, flex: 1, flexWrap: 'wrap' }}>
               From: {news_site_long}
+            </Text>
+            <Text style={{ marginBottom: 10, fontSize: 14, flex: 1, flexWrap: 'wrap' }}>
+              {moment.unix(date_published).fromNow()}
             </Text>
             <Text style={{ marginBottom: 10, flex: 1, flexWrap: 'wrap' }}>{title}</Text>
             <Button
@@ -29,6 +33,9 @@ export default class ArticleCard extends Component {
                 marginLeft: 'auto',
               }}
               textStyle={{ color: primaryColor }}
+              onPress={() => {
+                Linking.openURL(url);
+              }}
             />
           </View>
         </View>
