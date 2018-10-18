8bd5d9416ee9532f1f7be80090836012a0a15ecc
Rakibul Hasan <xenax.rakibul@gmail.com>
2018-10-18 21:47:24 +0600

    Fix: Launch Details

--- src/screens/HomeScreen/LaunchDetails.js
+++ src/screens/HomeScreen/LaunchDetails.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { ScrollView, Text, RefreshControl, View } from 'react-native';
-import { Card, Badge } from 'react-native-elements';
+import { ScrollView, Text, RefreshControl, View, Image, Linking } from 'react-native';
+import { Badge, Button } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import moment from 'moment';
 
 import { getSingleLaunch } from '../../redux/singleLaunch';
-import { white, success } from '../../colors';
+import { white, success, black, primaryColor, danger } from '../../colors';
 
 class LaunchDetails extends Component {
   static navigationOptions = () => ({
@@ -74,7 +74,7 @@ class LaunchDetails extends Component {
 
   render() {
     const { singleLaunch } = this.props;
-    const { name, rocket, missions } = this.props.singleLaunch.data;
+    const { name, rocket, missions, vidURLs } = this.props.singleLaunch.data;
     const { days, hours, minutes, seconds } = this.state;
     return (
       <ScrollView
@@ -83,35 +83,158 @@ class LaunchDetails extends Component {
         }
         style={{ backgroundColor: white }}
       >
-        <Card image={{ uri: rocket && rocket.imageURL }} containerStyle={{ borderRadius: 5 }}>
-          <Text style={{ marginBottom: 10, fontSize: 24 }}>{name}</Text>
-          <Text style={{ marginBottom: 10 }}>
-            {missions && !!missions.length && missions[0].description}
-          </Text>
+        {rocket &&
+          rocket.imageURL && <Image style={{ height: 200 }} source={{ uri: rocket.imageURL }} />}
+        <View style={{ padding: 10 }}>
+          <Text style={{ marginBottom: 10, fontSize: 24, textAlign: 'center' }}>{name}</Text>
           <View
             style={{
               flex: 1,
               alignItems: 'center',
               justifyContent: 'center',
+              marginVertical: 10,
             }}
           >
             <Badge
               containerStyle={{
                 backgroundColor: success,
                 borderRadius: 5,
-                marginBottom: 10,
               }}
             >
               <Text style={{ padding: 5, fontSize: 18, color: white }}>Launching in..</Text>
             </Badge>
           </View>
-          <Text style={{ marginBottom: 10, fontSize: 26, textAlign: 'center' }}>
-            <Text>{!!days && `${days}d : `}</Text>
-            <Text>{`${hours}h : `}</Text>
-            <Text>{`${minutes}m : `}</Text>
-            <Text>{`${seconds}s`}</Text>
-          </Text>
-        </Card>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ marginHorizontal: 5 }}>
+              <Text style={{ fontSize: 46, textAlign: 'center' }}>{days}:</Text>
+              <Text style={{ textAlign: 'center' }}>Days</Text>
+            </View>
+            <View style={{ marginHorizontal: 5 }}>
+              <Text style={{ fontSize: 46, textAlign: 'center' }}>{hours}:</Text>
+              <Text style={{ textAlign: 'center' }}>Hours</Text>
+            </View>
+            <View style={{ marginHorizontal: 5 }}>
+              <Text style={{ fontSize: 46, textAlign: 'center' }}>{minutes}:</Text>
+              <Text style={{ textAlign: 'center' }}>Minutes</Text>
+            </View>
+            <View style={{ marginHorizontal: 5 }}>
+              <Text style={{ fontSize: 46, textAlign: 'center' }}>{seconds}</Text>
+              <Text style={{ textAlign: 'center' }}>Seconds</Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            {vidURLs &&
+              vidURLs.map(item => (
+                <Button
+                  key={item}
+                  icon={{ name: 'live-tv', type: 'material', color: danger }}
+                  title="Live"
+                  buttonStyle={{
+                    backgroundColor: white,
+                    borderColor: danger,
+                    borderWidth: 0,
+                    borderRadius: 5,
+                    padding: 5,
+                  }}
+                  containerViewStyle={{
+                    marginLeft: 0,
+                  }}
+                  textStyle={{ color: danger }}
+                  onPress={() => {
+                    Linking.openURL(item);
+                  }}
+                />
+              ))}
+          </View>
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginVertical: 10,
+            }}
+          >
+            <Badge
+              containerStyle={{
+                backgroundColor: success,
+                borderRadius: 5,
+              }}
+            >
+              <Text style={{ padding: 5, fontSize: 18, color: white }}>Mission</Text>
+            </Badge>
+          </View>
+          {missions &&
+            missions.map(item => (
+              <View key={item.id}>
+                <Text style={{ marginBottom: 10, fontSize: 18, color: black, textAlign: 'center' }}>
+                  {item.name}
+                </Text>
+                <Text style={{ marginBottom: 10 }}>{item.description}</Text>
+              </View>
+            ))}
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginVertical: 10,
+            }}
+          >
+            <Badge
+              containerStyle={{
+                backgroundColor: success,
+                borderRadius: 5,
+              }}
+            >
+              <Text style={{ padding: 5, fontSize: 18, color: white }}>Rocket</Text>
+            </Badge>
+          </View>
+          <View>
+            <Text style={{ marginBottom: 10, fontSize: 18, color: black, textAlign: 'center' }}>
+              {rocket && rocket.name}
+            </Text>
+            <Text>Configuration: {rocket && rocket.configuration}</Text>
+            <Text>Family: {rocket && rocket.familyname}</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+              <Button
+                icon={{ name: 'wikipedia-w', type: 'font-awesome', color: primaryColor }}
+                title="WIKI"
+                buttonStyle={{
+                  backgroundColor: white,
+                  borderColor: primaryColor,
+                  borderWidth: 0,
+                  borderRadius: 5,
+                  padding: 5,
+                }}
+                containerViewStyle={{
+                  marginLeft: 0,
+                }}
+                textStyle={{ color: primaryColor }}
+                onPress={() => {
+                  Linking.openURL(rocket.wikiURL);
+                }}
+              />
+              <Button
+                icon={{ name: 'info', type: 'font-awesome', color: primaryColor }}
+                title="INFO"
+                buttonStyle={{
+                  backgroundColor: white,
+                  borderColor: primaryColor,
+                  borderWidth: 0,
+                  borderRadius: 5,
+                  padding: 5,
+                }}
+                containerViewStyle={{
+                  marginLeft: 0,
+                }}
+                textStyle={{ color: primaryColor }}
+                onPress={() => {
+                  Linking.openURL(rocket.infoURL);
+                }}
+              />
+            </View>
+          </View>
+        </View>
       </ScrollView>
     );
   }
